9184736b126240e001bc43cbb415c70b83a419d5
Wejil <118178461+Wejil@users.noreply.github.com>
2026-02-17 13:00:34 +0100

    Esercizi

---
+++ Corso_Python_Febbraio/12_Martedi_17/Mattina/Esercizio.py
@@ -0,0 +1,12 @@
+'''
+Scrivete un programma che utilizza il cifrario di Cesare per criptare una
+parola o decriptarla.
+Il Cifrario di Cesare è un algoritmo di crittografia che consiste nello spostare
+ciascuna lettera di una certa quantità di posti nell'alfabeto. Per utilizzarlo, si
+sceglie una chiave (scelta dall'utente) che rappresenta il numero di posti
+di cui ogni lettera dell'alfabeto verrà spostata: ad esempio, se si sceglie
+una chiave di 3, la lettera A diventerà D, la lettera B diventerà E e così via.
+Per decifrare un messaggio cifrato con il cifrario di Cesare bisogna
+conoscere la chiave utilizzata e spostare ogni lettera indietro di un numero
+di posti corrispondente alla chiave.
+'''
